1da44e687bcecf523bf2b386f478a94c566ef948
NSP <106930910+kishinsagi@users.noreply.github.com>
2023-06-24 18:57:41 +0000

    Add script files and sensitive words

---
+++ bash/cw.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "cURLWall is starting..."
+sleep 2s
+addr = $1
+pathFile = path.txt
+lexiconFile = lexicon.txt
+userAgent = "Dalvik/1.1"
+stay = 1
+timeout = 1 
+
+function updRand {
+	let junk1 = $RANDOM * 32768 + $RANDOM
+	let junk2 = $RANDOM * 32768 + $RANDOM
+}
+
+if [ "$1" == "" ] ; then
+	echo "cURLWall cannot launch: no specified address."
+fi
+
+if [ "$1" != "" ] ; then
+	echo "Targeted address: $addr"
+	echo "Reading from files..."
+	while IFS= read -r lexicon; do
+		while IFG= read -r path; do
+			clear
+			pathText = ""
+			pathText = ${path//(lexicon)/$lexicon}
+			urlText = ${pathText//(domain)/$addr}
+			let stay = $RANDOM / 8192
+			echo "User agent: $userAgent"
+			echo "Accessing: $urlText"
+			echo "Targeted host: $addr"
+			echo "Wait: $timeout second(s). Stay: $stay second(s)."
+			echo ""
+			curl -GA "$userAgent" -4m $stay --connect-timeout $timeout "$urlText" > /dev/null
+			curl -GA "$userAgent" -6m $stay --connect-timeout $timeout "$urlText" > /dev/null
+		done < ./$pathFile
+	done < ./$lexiconFile
+	echo "Job complete."
+fi
+
+echo "Terminating..."
+exit
